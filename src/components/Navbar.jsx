3bb13e7fbe32fe9dfd4475ae13baf1b036ce67a1
Weverson Diazz <63312710+wevdiaz@users.noreply.github.com>
2022-09-06 20:59:36 -0300

    estilização global index.css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,22 @@
 import { Link } from "react-router-dom";
+import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
 
 export function Navbar() {
   return (
     <nav id="navbar">
       <h2>
-        <Link to="/" >MoviesLib</Link>
+        <Link to="/" >
+          <BiCameraMovie />
+          MoviesLib
+        </Link>
       </h2>
 
-      <p>
-        <Link to="movie/1">Movies</Link>
-      </p>
-      <p>
-        <Link to="search">Search</Link>
-      </p>
+      <form>
+        <input type="text" placeholder="Busque um filme" />
+        <button type="submit">
+          <BiSearchAlt2 />
+        </button>
+      </form>
     </nav>
   );
 }
