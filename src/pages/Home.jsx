31bf938d99caa38f2ae289bd4ceb935305c45249
Weverson Diazz <63312710+wevdiaz@users.noreply.github.com>
2022-09-06 21:27:07 -0300

    pegando dados da API

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,30 @@
+import { useState, useEffect } from "react";
 
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
 
 export function Home() {
+  const [topMovies, setTopMovies] = useState([]);
+
+  const getTopRatedMovies = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    setTopMovies(data.results);
+  }
+
+  useEffect(() => {
+    const topRatedUrl = `${moviesURL}top_rated?${apiKey}`;
+
+    getTopRatedMovies(topRatedUrl);
+
+  }, []);
+
   return (
-    <div>Home</div>
+    <div>
+      Home
+
+      
+    </div>
   );
 }
