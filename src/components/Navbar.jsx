4baf005fe7f01c58434f905cc04e9b8a9bb7087e
Weverson Diazz <63312710+wevdiaz@users.noreply.github.com>
2022-09-07 17:13:52 -0300

    styles sidebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
 
+import  "./navbar.css";
+
 export function Navbar() {
   return (
     <nav id="navbar">
