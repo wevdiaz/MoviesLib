b302485d50006a9376d28623bcf70303535e8a9f
Weverson Diazz <63312710+wevdiaz@users.noreply.github.com>
2022-09-08 20:31:21 -0300

    direcionando para o search

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,22 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
 
 import  "./navbar.css";
 
 export function Navbar() {
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    if (!search) return;
+
+    navigate(`/search?q=${search}`);
+    setSearch("");
+  }
+
   return (
     <nav id="navbar">
       <h2>
@@ -13,8 +26,13 @@ export function Navbar() {
         </Link>
       </h2>
 
-      <form>
-        <input type="text" placeholder="Busque um filme" />
+      <form onSubmit={handleSubmit}>
+        <input 
+            type="text" 
+            placeholder="Busque um filme" 
+            onChange={(e) => setSearch(e.target.value)} 
+            value={search}
+        />
         <button type="submit">
           <BiSearchAlt2 />
         </button>
